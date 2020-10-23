0bf0281aebbb321eaece4460bf9b0f9c87b58913
jstar34point5x2 <Kinitawowi12@gmail.com>
2020-10-22 22:44:27 -0500

    logging off for the night

--- app/src/main/java/com/blinky/devprofile/MainActivity.kt
+++ app/src/main/java/com/blinky/devprofile/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.blinky.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create bitmap for rounded edges on corners
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+
+        logoImageView.setImageDrawable(rounded)
+
+        // could make circular instead
+        // rounded.isCircular = true
     }
 }
